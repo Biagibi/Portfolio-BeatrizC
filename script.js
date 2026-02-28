a4c740d003e8de606a957876a723d7cd6e059a6b
Biagibi <162746216+Biagibi@users.noreply.github.com>
2026-02-28 00:44:07 -0300

    Alterações de titulos

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-const circles = document.querySelectorAll('.circle');
-
-circles.forEach(circle => {
-  const percent = circle.getAttribute('data-percent');
-  const img = circle.getAttribute('data-img'); // pega o caminho da imagem
-  const span = circle.querySelector('span');
-
-  // Aplica a imagem no span
-  span.style.setProperty('--img-url', `url('${img}')`);
-
-  // Preenche o círculo de acordo com a porcentagem
-  circle.style.background = `conic-gradient(#355654 ${percent}%, #1a2527 ${percent}%)`;
-
-  // Efeito de animação ao passar o mouse
-  circle.addEventListener('mouseenter', () => {
-    let current = 0;
-    const interval = setInterval(() => {
-      if (current >= percent) {
-        clearInterval(interval);
-      } else {
-        current++;
-        circle.style.background = `conic-gradient(#355654 ${current}%, #1a2527 ${current}%)`;
-      }
-    }, 15);
-  });
-});
-
-
-c6cccc
+function virarCard(card) {
+  card.classList.toggle("virado")
+}
